6f911021b59f391a6f6f8c28508b9b1f9a6afbc0
J-D-J-Clare <jclare2@wisc.edu>
2023-09-28 17:00:12 -0600

    Update Mods.R

--- Mods.R
+++ Mods.R
@@ -1,3 +1,25 @@
+###A few pre-emptive notes:
+###1) On the HPC cluster used (with one chain per node), the reported sampling parameters
+###   (70000 iterations, 50000 burn-in, thinning of 5) was generally sufficient to get an adequate 
+###   posterior sample for models *without* latent variables across 3 chains.
+###2) For the more complex models presented below, we effectively had to implement multiple iterative runs
+###   per "chain" (e.g., submit job, save samples, use final samples as initial values for a new run on same 
+###   "chain; see http://danielturek.github.io/public/restartingMCMC/restartingMCMC.html). Based on the number
+###   of "re-runs" needed, our (my) guess is that a usable posterior sample run all-the-way-through
+###   would involve 4+ chains with burn-in of 100000-150000 iterations, 200-250 thousand total iterations 
+###   and some amount of thinning to store reasonably. 
+###3) In practice, we did not check whether the factors or loadings were uniquely identified, and
+###   we do not implement constraints to ensure these are uniquely identified. (Both can be
+###   multi-modal, there's potential for label-switching across chains, etc.). We did check whether
+###   the derived residual covariance matrix for species mixed well (e.g., crossprod(lambda)). Our experience 
+###   is that if the residual covariance matrix is well-mixed and identified, then the specific factors and 
+###   loadings are well identified after rotation/sign/permuation corrections.
+###   (see https://cran.r-project.org/web/packages/factor.switching/index.html)
+
+
+
+
+
 load("NimbleInputs8.RData")
 library(nimble)
 library(nimbleEcology)
@@ -138,12 +160,6 @@ Mod39Conf$addSampler(target = 'sig_b[24]', type= 'RW', control=list(log=TRUE))
 Rmcmc<-buildMCMC(Mod39Conf)
 compMCMC <- compileNimble(Rmcmc, Mod39)
 
-
-###In practice, given HPC contstraints, we were able to run chains for about 70,000 iterations per job,
-###with one chain per node. We stored the final samples, and used these as starting values for subsequent runs.
-###In an ideal world, doing all of this at once (maybe in parallel), a useful posterior sample for this model
-###might involve 6 chains with a burn-in of ~150000 iterations, niter=200000+, and thin=20 or so.
-
 samps<-runMCMC(mcmc = compMCMC$Rmcmc,
                niter=70000, nburnin=50000, thin=10, 
                nchains=1) 
@@ -231,6 +247,31 @@ MA1_P1<-nimbleCode({
 
 
 ###Data and constants are the same as for M39.
+
+Constants<-list(Ends=ifelse(N_Surveys>5, 5, N_Surveys),
+                nspec=185, nsites=320, Starts2=matrix(1, 320, 2), 
+                X1=cbind(rep(1, 320),
+                         as.numeric(scale(Site_Data2$MeanHistoricPPT)),
+                         as.numeric(scale(Site_Data2$MeanHistoricTMin)),
+                         as.numeric(scale(Site_Data2$HistoricTMinFPC2)),
+                         as.numeric(scale(Site_Data2$Prop_Ag_Hist)),
+                         as.numeric(scale(Site_Data2$PropUrbanHist)),
+                         as.numeric(scale(Site_Data2$MeanHistoricTMin))^2,
+                         as.numeric(scale(Site_Data2$HistoricTMinFPC2))^2),
+                X2=cbind(rep(1, 320),
+                         as.numeric(scale(Site_Data2$MeanModernPPT)),
+                         as.numeric(scale(Site_Data2$MeanModernTMin)),
+                         as.numeric(scale(Site_Data2$ModernTMinFPC2)),
+                         as.numeric(scale(Site_Data2$PropAgMod)),
+                         as.numeric(scale(Site_Data2$PropUrbanMod)),
+                         as.numeric(scale(Site_Data2$MeanModernTMin))^2,
+                         as.numeric(scale(Site_Data2$ModernTMinFPC2))^2,
+                         as.numeric(scale(Site_Data2$MeanModernPPT-Site_Data2$MeanHistoricPPT)),
+                         as.numeric(scale(Site_Data2$MeanModernTMin-Site_Data2$MeanHistoricTMin)),
+                         as.numeric(scale(Site_Data2$ModernTMinFPC2-Site_Data2$HistoricTMinFPC2))))
+
+Data<-list(y=y[, , 1:5, ])
+
 ###here are some initial values that are guaranteed to start sampling.
 Inits <- list(y=yIn[,,1:5,], mu_b=rep(0, 19), sig_b=rep(.25, 19), 
               a=matrix(0, 2, 185), mu_a=rep(0, 2), sig_a=rep(.25, 2),
@@ -362,5 +403,140 @@ Mod34Conf$addSampler(target="sig_b[13]", type="RW", control=list(log=TRUE))
 Rmcmc<-buildMCMC(Mod34Conf)
 compMCMC <- compileNimble(Rmcmc, Mod34)
 samps<-runMCMC(mcmc = compMCMC$Rmcmc,
-               niter=70000, nburnin=60000, thin=5, 
+               niter=70000, nburnin=50000, thin=5, 
                nchains=1)
+
+
+###Model 21
+
+M21<-nimbleCode({
+  for (e in 1:2){ ###detection 'fixed' effects
+    mu_a[e]~dnorm(0, sd=1) 
+    sig_a[e]~T(dnorm(0, sd = 1), 0, )
+    for (i in 1:nspec){
+      a[e,i]~dnorm(mu_a[e], sd=sig_a[e])
+    }
+  }
+  
+  sig_det~T(dnorm(0, sd = 1), 0, ) 
+  
+  for (i in 1:8){
+    mu_b[i]~dnorm(0, sd=0.75)
+    sig_b[i]~T(dnorm(0, sd = 0.75), 0, )
+    for (s in 1:nspec){
+      b[i, s]~dnorm(mu_b[i], sd=sig_b[i])
+    }
+  }
+
+ mu_gamma~dnorm(0, sd=0.75)
+ sig_gamma~T(dnorm(0, sd=0.75), 0,)
+ mu_delta~dnorm(0, sd=0.75)
+ sig_delta~T(dnorm(0, sd=0.75), 0,)
+ 
+for (i in 1:nspec){
+   gamdiff[i]~dnorm(mu_gamma, sd=sig_gamma)
+   phidiff[i]~dnorm(mu_delta, sd=sig_delta)
+  ###these terms describe additive differences between psi,
+  ###gamma (colonization), and phi (persistence)
+ }
+
+  theta[1]~dgamma(10, 1)
+  theta[2]~dgamma(10, 1)
+  theta[3]~dgamma(10, 1)
+  
+  for (k in 1:3){
+    tau[k]<-prod(theta[1:k])
+    for (i in 1:nspec){
+      lambda[i, k]~dt(0, tau=tau[k], 3)
+    }  
+  }  
+
+  
+    for (s in 1:nsites){
+      for (i in 1:nspec){
+      psi[i, s] <- iprobit(inprod(b[1:8, i], X1[s,1:8])+inprod(lambda[i, 1:3], eta[s, 1:3, 1]))
+      gam[i, s] <- iprobit(inprod(b[1:8, i], X2[s,1:8])+gamdiff[i]+inprod(lambda[i, 1:3], eta[s, 1:3, 2]))
+      phi[i, s] <- iprobit(inprod(b[1:8, i], X2[s, 1:8])+phidiff[i]+inprod(lambda[i, 1:3], eta[s, 1:3, 2]))     
+      y[s,1:2,1:5,i]~dDynOcc_ssm(init=psi[i, s], probPersist=phi[i, s],
+                           probColonize = gam[i, s], p=p[s,1:2, 1:5,i],
+                           start=Starts2[s,1:2], end=Ends[s,1:2]) ###note, new ends.some=0
+      }
+    }
+  
+for (s in 1:nsites){
+    for (e in 1:2){
+      lambda[s, 1, e]~dnorm(0, 1)
+      lambda[s, 2, e]~dnorm(0, 1)
+      lambda[s, 3, e]~dnorm(0, 1)
+      for (j in 1:5){
+        eps[s, e, j]~dnorm(0, sd=sig_det)
+        for (i in 1:nspec){
+          logit(p[s, e, j, i])<-eps[s, e, j]+a[e, i] 
+        }
+      }
+    }
+  }
+        
+})
+
+
+
+###Model 19
+
+
+MA1_P1<-nimbleCode({
+  for (e in 1:2){ ###detection 'fixed' effects
+    mu_a[e]~dnorm(0, sd=1) 
+    sig_a[e]~T(dnorm(0, sd = 1), 0, )
+    for (i in 1:nspec){
+      a[e,i]~dnorm(mu_a[e], sd=sig_a[e])
+    }
+  }
+  
+  sig_det~T(dnorm(0, sd = 1), 0, ) 
+  
+  for (i in 1:8){
+    mu_b[i]~dnorm(0, sd=0.75)
+    sig_b[i]~T(dnorm(0, sd = 0.75), 0, )
+    for (s in 1:nspec){
+      b[i, s]~dnorm(mu_b[i], sd=sig_b[i])
+    }
+  }
+ 
+ theta[1]~dgamma(10, 1)
+ theta[2]~dgamma(10, 1)
+ theta[3]~dgamma(10, 1)
+   
+ for (k in 1:3){
+   tau[k]<-prod(theta[1:k])
+    for (i in 1:nspec){
+    lambda[i, k]~dt(0, tau=tau[k], 3)
+    }  
+  }
+    for (s in 1:nsites){
+      for (i in 1:nspec){
+      psi[i, s, 1] <- iprobit(inprod(b[1:8, i], X1[s,1:8])+inprod(lambda[i, 1:3], eta[s, 1:3, 1]))
+      psi[i, s, 2] <- iprobit(inprod(b[1:8, i], X2[s,1:8])+inprod(lambda[i, 1:3], eta[s, 1:3, 2]))
+      y[s,1:2,1:5,i]~dDynOcc_ssm(init=psi[i, s, 1], probPersist=psi[i, s, 2],
+                           probColonize = psi[i, s, 2], p=p[s,1:2, 1:5,i],
+                           start=Starts2[s,1:2], end=Ends[s,1:2]) ###note, new ends.some=0
+      }
+    }
+  
+  for (s in 1:nsites){
+    for (e in 1:2){
+      eta[s, 1, e]~dnorm(0, 1)
+      eta[s, 2, e]~dnorm(0, 1)
+      eta[s, 3, e]~dnorm(0, 1)
+      for (j in 1:5){
+        eps[s, e, j]~dnorm(0, sd=sig_det)
+          for (i in 1:nspec){
+           logit(p[s, e, j, i])<-eps[s, e, j]+a[e, i] 
+          }
+      }
+    }
+  }
+        
+      
+})
+
